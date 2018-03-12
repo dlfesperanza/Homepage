6f568e15d1fc4d98ea9fd6d8268a6ed8ae0f5839
dlfesperanza <dfesperanza@up.edu.ph>
2018-03-12 17:56:25 +0800

    Initial homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import File from './File';
+
+class App extends Component {
+  render() {
+    return (
+      <File placeholder = "Filter courses"/>
+    );
+  }
+}
+
+export default App;
